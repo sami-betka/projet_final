02785cbd395e926b91cdefd33854054d78a082e6
sami-betka <sami1206@hotmail.fr>
2020-01-28 11:17:30 +0100

    c

---
+++ Turf/src/main/java/turf/TurfApplication.java
@@ -0,0 +1,13 @@
+package turf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurfApplication.class, args);
+	}
+
+}
---
+++ Turf/src/test/java/turf/TurfApplicationTests.java
@@ -0,0 +1,13 @@
+package turf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
